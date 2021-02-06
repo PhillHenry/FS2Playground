fbc29a0aa9d324f636ee0d784bd6440b7a6850a7
Phillip Henry <PhillHenry@gmail.com>
2021-02-06 17:45:30 +0000

    Hmm, apparently the fs2-kafka library (2.x) is not compatible with fs2 3.x.

--- modules/core/src/test/scala/uk/co/odinconsultants/KafkaSuite.scala
+++ modules/core/src/test/scala/uk/co/odinconsultants/KafkaSuite.scala
@@ -16,9 +16,12 @@
 
 package uk.co.odinconsultants
 
+import cats.effect.Sync
+import fs2.kafka.{ConsumerSettings, KafkaDeserializer, KafkaSerializer}
 import munit.CatsEffectSuite
-import net.manub.embeddedkafka.EmbeddedKafkaConfig
 import net.manub.embeddedkafka.EmbeddedKafka._
+import net.manub.embeddedkafka.EmbeddedKafkaConfig
+import org.apache.kafka.clients.consumer.ConsumerConfig
 
 import java.util.UUID
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
@@ -44,4 +47,52 @@ class KafkaSuite extends CatsEffectSuite {
         ) ++ props
       )
     )(f(_, nextTopicName()))
+
+  final def consumerProperties(
+      config: EmbeddedKafkaConfig
+  ): Map[String, String] =
+    Map(
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> s"localhost:${config.kafkaPort}",
+      ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "earliest",
+      ConsumerConfig.GROUP_ID_CONFIG -> "group"
+    )
+
+  final def consumerSettings[F[_]](
+      config: EmbeddedKafkaConfig
+  )(implicit F: Sync[F]): ConsumerSettings[F, String, String] =
+    ConsumerSettings[F, String, String]
+      .withProperties(consumerProperties(config))
+      .withRecordMetadata(_.timestamp.toString)
+
+  implicit final val stringDeserializer: KafkaDeserializer[String] =
+    new org.apache.kafka.common.serialization.StringDeserializer
+
+  implicit final val stringSerializer: KafkaSerializer[String] =
+    new org.apache.kafka.common.serialization.StringSerializer
+
+  test("KafkaConsumer#stream should consume all records with subscribe") {
+    withKafka(
+      Map.empty,
+      { (config, topic) =>
+        createCustomTopic(topic, partitions = 3)
+        val produced = (0 until 5).map(n => s"key-$n" -> s"value->$n")
+        publishToKafka(topic, produced)
+
+//      val consumed =
+//        KafkaConsumer.stream[IO]
+//          .using(consumerSettings(config))
+//          .evalTap(_.subscribeTo(topic))
+//          .evalTap(consumer => IO(assert(consumer.toString.startsWith("KafkaConsumer$"))).void)
+//          .evalMap(IO.sleep(3.seconds).as) // sleep a bit to trigger potential race condition with _.stream
+//          .flatMap(_.stream)
+//          .map(committable => committable.record.key -> committable.record.value)
+//          .interruptAfter(10.seconds) // wait some time to catch potentially duplicated records
+//          .compile
+//          .toVector
+//          .unsafeRunSync
+//
+//      assertEquals(consumed.toSet, produced.toSet)
+      }
+    )
+  }
 }
