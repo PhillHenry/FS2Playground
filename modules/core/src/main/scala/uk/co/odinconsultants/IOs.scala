4d11b34a5e1de0a0e236bc8bdeb08144408b0918
Phillip Henry <PhillHenry@gmail.com>
2021-02-08 13:20:37 +0000

    Cancelling a fibre.

---
+++ modules/core/src/main/scala/uk/co/odinconsultants/IOs.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Phillip Henry
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.co.odinconsultants
+
+import cats.effect.IO
+
+object IOs {
+
+  val helloWorld: IO[Unit] = IO { println("Hello, world") }
+
+  def printOut(x: Any): IO[Unit] = IO { println(s"printOut: $x") }
+
+  val timeMs: IO[Long] = IO { System.currentTimeMillis() }
+
+  val blockingSleep1s: IO[Unit] = IO {
+    val ms = 1000
+    println(s"Sleeping for $ms ms...")
+    try {
+      Thread.sleep(ms)
+    } catch {
+      case x: InterruptedException => println(s"Interrupted ${x.getMessage}")
+    }
+  }
+
+}
